6a02a511c715e6d0c989ef0b73e356a54d6485b2
201611102 장효진 <jhj0443@naver.com>
2017-03-21 16:03:35 +0900

    Create Duck.java

---
+++ j/src/headfirst/strategy/Duck.java
@@ -0,0 +1,33 @@
+package headfirst.strategy;
+public abstract class Duck{
+  FlyBehavior flyBehavior;
+  //QuackBehavior quackBehavior;
+
+  public Duck(){
+  }
+
+  public void setFlyBehavior (FlyBehavior fb){
+   flyBehavior = fb;
+  }
+  /*
+  public void setQuackBehavior(QuackBehavior qb){
+    quackBehavior = qb;
+  }
+  */
+  abstract void display();
+  
+  public void performFly(){
+    flyBehavior.fly();
+    System.out.println("Duck fly...");
+  }
+  /*
+  public void performQuack(){
+    quackBehavior.quack();
+  }
+  */
+  
+  public void swim(){
+    System.out.println("All ducks float, even decoys!");
+  }
+  
+}
