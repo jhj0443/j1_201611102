5ea552a213dd63040eb9e1e4c4caa4fb83c4f9b5
201611102 장효진 <jhj0443@naver.com>
2017-05-16 13:13:27 +0900

    Create Singleton.java

---
+++ j/src/headfirst/singleton/classic/Singleton.java
@@ -0,0 +1,20 @@
+package headfirst.singleton.classic;
+
+public class Singleton{
+  private static Singleton uniqueInstance;
+  private static int numCalled=0;
+  
+  private Singleton(){
+    
+    
+  }
+  public static Singleton getInstance(){
+    if(uniqueInstance==null){
+      uniqueInstance = new Singleton();
+      System.out.println("Creating unique instance of Chocolate Boiler");
+    }
+      System.out.println("Returning instance of Chocolate Bolier");
+      System.out.println("jhj : numCalled "+numCalled++);
+      return uniqueInstance;  
+  }
+}
